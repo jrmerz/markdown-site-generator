a2405f2abe7eeeed6fd6854e3ccd9faa9284a40b
Justin Merz <jrmerz@gmail.com>
2015-10-04 23:29:40 -0700

    bug fixes, adding watch to directory for auto builds

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ var generate = require('./lib/generate');
 var bower = require('./lib/bower');
 var path = require('path');
 var fs = require('fs');
+var watch = require('watch');
 
 var dir = process.argv[2];
 
-var root = path.join(__dirname, dir);
-var build = 'build';
+var root = path.join(process.cwd(), dir);
+var buildDir = 'build';
 
 var config = {};
 if( fs.existsSync(path.join(root, '.config')) ) {
   config = eval('(' + fs.readFileSync(path.join(root, '.config'), 'utf-8') +' )');
 }
+
+if( config.build ) {
+  buildDir = config.build;
+}
+config.buildDir = buildDir;
 
 if( config.ignore ) {
   for( var i = 0; i < config.ignore.length; i++ ) {
@@ -21,14 +27,49 @@ if( config.ignore ) {
   }
 }
 
-rimraf(path.join(root, build), function(err){
-  fs.mkdirSync(path.join(root, build));
+config.buildPath = path.join(root, buildDir);
+config.root = root;
+config.log = function(msg) {
+  if( this.silent ) return;
+  console.log(msg);
+};
 
-  crawler.run(root, config, function(err, data){
-    generate.run('../website/build', data, function(){
-      bower.install(root, build, config, function(){
-        console.log('done');
+// setup watcher
+var ignore = ['bower_components', '.git', buildDir];
+watch.watchTree(root,
+  {
+    filter : function(file, dir) {
+      var name = file.replace(/.*\//,'');
+      if( ignore.indexOf(name) > -1 ) {
+        return false;
+      }
+      return true;
+    }
+  },
+  function (f, curr, prev) {
+      build();
+  }
+);
+
+function build() {
+  config.log('***** BUILDING *****');
+  config.log(config.root);
+
+  rimraf(config.buildPath, function(err){
+    fs.mkdirSync(config.buildPath);
+
+    crawler.run(config, function(err, data){
+      config.log('Generating content in: '+config.buildPath)
+      generate.run(config.buildPath, data, function(){
+        bower.install(config, function(){
+          console.log('Done.');
+        });
       });
     });
   });
-});
+}
+
+global.config = config;
+global.log = function(msg) {
+
+}
